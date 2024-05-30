1a2a009055f7b734f3c5c6f64d7421d449f3f7bd
moojor224 <moojor224@gmail.com>
2024-05-30 16:08:39 -0500

    fix some things

--- src/extension.ts
+++ src/extension.ts
@@ -2,11 +2,12 @@ import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
 
-	let command = vscode.commands.registerTextEditorCommand('html-to-js.insertFunction', (editor: vscode.TextEditor, edit: vscode.TextEditorEdit) => {
+    let command = vscode.commands.registerTextEditorCommand('repo_name.command', (editor: vscode.TextEditor, edit: vscode.TextEditorEdit) => {
         // do things here
-	});
-    
-	context.subscriptions.push(command);
+        vscode.window.showInformationMessage('command has been run');
+    });
+
+    context.subscriptions.push(command);
 }
 
 export function deactivate() { }
